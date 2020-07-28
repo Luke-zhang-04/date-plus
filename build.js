f343cd20e6b585bf8d8b6ded7c67a1d1781e007d
Luke <luke.zhang2004dev@gmail.com>
2020-07-28 13:06:18 -0400

    fix codeclimate config (#6)
    - bump version to 2.0.3 e755d8c

--- build.js
+++ build.js
@@ -6,7 +6,7 @@
  * @copyright Copyright (C) 2020 Luke Zhang
  * @author Luke Zhang luke-zhang-04.github.io
  * @license MIT
- * @version 2.0.2
+ * @version 2.0.3
  * @file Replaces comments with exessive newlines with one newline
  */
 
