1b4a0d28497b202f6cf31146cec8233d6bb096a8
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-02-24 18:18:43 -0500

    fix typings, add typings for cjs
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,7 @@
  * @author Luke Zhang luke-zhang-04.github.io
  * @license MIT
  * @version 3.0.0
- * @exports Elapse
- * @file defines Elapse class for elapsed
  */
 
 export * from "./lib"
+export {default} from "./lib"
